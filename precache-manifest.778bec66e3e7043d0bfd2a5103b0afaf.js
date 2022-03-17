c911dbd51a4b40765677e5d6b2a8c2d86070237a
PhamJimmy <JimmyPhamRes@gmail.com>
2022-03-17 11:40:44 -0700

    Updates

---
+++ precache-manifest.778bec66e3e7043d0bfd2a5103b0afaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd5cc630efec833c2d165afeee4c4238",
+    "url": "/recipe-tracker/index.html"
+  },
+  {
+    "revision": "41390b8c72a9993bf93d",
+    "url": "/recipe-tracker/static/css/main.ec7bfdb3.chunk.css"
+  },
+  {
+    "revision": "8446c857f19c1a0902b8",
+    "url": "/recipe-tracker/static/js/2.91e1f410.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/recipe-tracker/static/js/2.91e1f410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41390b8c72a9993bf93d",
+    "url": "/recipe-tracker/static/js/main.a5b0500c.chunk.js"
+  },
+  {
+    "revision": "982fa5ef2b198456df9f",
+    "url": "/recipe-tracker/static/js/runtime-main.246dc71b.js"
+  }
+]);
